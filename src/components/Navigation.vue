8996f67212f31113542fde872a10d79e138c6ba2
natinael96 <natinael.96@gmail.com>
2026-05-27 22:46:35 +0300

    Enhance navigation component and styles: introduce new layout structure with nav-float-shell and nav-float-bar classes, update CSS for improved responsiveness, and refine dropdown menu functionality for better user experience.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,57 +1,61 @@
 <template>
   <nav class="nav-float" aria-label="Primary">
-    <a
-      href="#hero"
-      class="nav-float-pill nav-float-brand relative z-[1]"
-      @click.prevent="scrollTo('hero')"
-    >
-      <span class="relative z-[1]">NMS</span>
-    </a>
-
-    <div class="hidden md:flex nav-float-cluster" role="menubar">
-      <a
-        v-for="item in navItems"
-        :key="item.id"
-        :href="`#${item.id}`"
-        class="nav-float-btn"
-        role="menuitem"
-        @click.prevent="scrollTo(item.id)"
-      >
-        {{ item.label }}
-      </a>
-    </div>
-
-    <button
-      type="button"
-      class="nav-float-pill nav-float-menu md:hidden relative z-[1]"
-      :aria-expanded="mobileMenuOpen"
-      aria-label="Toggle menu"
-      @click="mobileMenuOpen = !mobileMenuOpen"
-    >
-      <Menu v-if="!mobileMenuOpen" :size="18" class="relative z-[1]" />
-      <X v-else :size="18" class="relative z-[1]" />
-    </button>
-
-    <Transition
-      enter-active-class="transition duration-300 ease-[cubic-bezier(0.22,1,0.36,1)]"
-      leave-active-class="transition duration-200 ease-[cubic-bezier(0.22,1,0.36,1)]"
-      enter-from-class="opacity-0 -translate-y-2 scale-[0.97]"
-      enter-to-class="opacity-100 translate-y-0 scale-100"
-      leave-from-class="opacity-100 translate-y-0 scale-100"
-      leave-to-class="opacity-0 -translate-y-1 scale-[0.98]"
-    >
-      <div v-if="mobileMenuOpen" class="nav-float-dropdown md:hidden">
+    <div class="nav-float-shell">
+      <div class="nav-float-bar">
         <a
-          v-for="item in navItems"
-          :key="item.id"
-          :href="`#${item.id}`"
-          class="nav-float-dropdown-link"
-          @click.prevent="onNavClick(item.id)"
+          href="#hero"
+          class="nav-float-pill nav-float-brand"
+          @click.prevent="scrollTo('hero')"
         >
-          {{ item.label }}
+          <span class="relative z-[1]">NMS</span>
         </a>
+
+        <div class="hidden md:flex nav-float-cluster" role="menubar">
+          <a
+            v-for="item in navItems"
+            :key="item.id"
+            :href="`#${item.id}`"
+            class="nav-float-btn"
+            role="menuitem"
+            @click.prevent="scrollTo(item.id)"
+          >
+            {{ item.label }}
+          </a>
+        </div>
+
+        <button
+          type="button"
+          class="nav-float-pill nav-float-menu md:hidden"
+          :aria-expanded="mobileMenuOpen"
+          aria-label="Toggle menu"
+          @click="mobileMenuOpen = !mobileMenuOpen"
+        >
+          <Menu v-if="!mobileMenuOpen" :size="18" class="relative z-[1]" />
+          <X v-else :size="18" class="relative z-[1]" />
+        </button>
       </div>
-    </Transition>
+
+      <Transition
+        enter-active-class="transition duration-300 ease-[cubic-bezier(0.22,1,0.36,1)]"
+        leave-active-class="transition duration-200 ease-[cubic-bezier(0.22,1,0.36,1)]"
+        enter-from-class="opacity-0 -translate-y-2 scale-[0.97]"
+        enter-to-class="opacity-100 translate-y-0 scale-100"
+        leave-from-class="opacity-100 translate-y-0 scale-100"
+        leave-to-class="opacity-0 -translate-y-1 scale-[0.98]"
+      >
+        <div v-if="mobileMenuOpen" class="nav-float-dropdown md:hidden">
+          <a
+            v-for="item in navItems"
+            :key="item.id"
+            :href="`#${item.id}`"
+            class="nav-float-dropdown-link"
+            @click.prevent="onNavClick(item.id)"
+          >
+            {{ item.label }}
+          </a>
+        </div>
+      </Transition>
+    </div>
   </nav>
 </template>
 
